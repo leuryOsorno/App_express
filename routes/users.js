422f57f30bc9eece1bb481b91851e38a9df9ba21
Leury Osorno <leury.osorno@correotdea.edu.co>
2023-08-11 21:10:06 -0500

    mas avance de classe

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const userService = require('../services/user.service');
+
 /* GET users listing. */
-router.get('/', (req, res)=> {
-  res.render('users/users',{title: 'Usuario'});
+router.get('/',async (req, res)=> {
+  const users = await userService.findUsers();
+  console.info('findUser',users);
+
+  res.render('users/users',{title: 'Usuario',userList:users});
 });
 
 module.exports = router;
