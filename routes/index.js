4eca579d33b801a3f84ae2ed3d40b509be5004a1
Leury Osorno <leury.osorno@correotdea.edu.co>
2023-08-11 19:36:32 -0500

    mas avance de classe

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azul' });
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Iniciar Sesión' });
+});
+
+router.post('/login', function(req, res, next) {
+  res.render('index', { title: 'Iniciar Sesión' });
 });
 
 module.exports = router;
